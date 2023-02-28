9565a24f93f09f7ba4529b9302f23a1d7b405a7c
Pineberry_Code <88601032+PineberryCode@users.noreply.github.com>
2023-02-28 18:31:26 -0500

    Update dbparking.sql

--- script_T-SQL/dbparking.sql
+++ script_T-SQL/dbparking.sql
@@ -3,29 +3,18 @@ use parking;
 
 create table automobile
 (
-	licensePlate char(5) not null,
-    identificationOwner int not null,
-    primary key (licensePlate)
+	licensePlate 		char(5) 	not null,
+    	identificationOwner 	int 		not null,
+    	
+	primary key (licensePlate)
 );
-alter table ticket add cost int not null;
+
 create table ticket
 (
-	automobile char(5) not null,
-    entry date not null,
-    constraint fk_automobile foreign key (automobile) references automobile (licensePlate),
-    primary key (automobile)
+	automobile 		char(5) 	not null,
+    	entry 			date 		not null,
+	cost 			int 		not null,
+	
+    	constraint fk_automobile foreign key (automobile) references automobile (licensePlate),
+    	primary key (automobile)
 );
-
-insert into automobile values ("ABS34",2346534);
-insert into ticket values ("ABS34","2022/04/10", 5);
-
-
-
-delete from ticket where automobile = "122d";
-delete from automobile where licensePlate = "122d";
-select * from automobile;
-select * from ticket;
-
-SELECT auto.licensePlate, auto.identificationOwner, tckt.entry, tckt.cost
-                        FROM ticket tckt INNER JOIN automobile auto
-                        ON tckt.automobile = auto.licensePlate order by tckt.entry DESC;
